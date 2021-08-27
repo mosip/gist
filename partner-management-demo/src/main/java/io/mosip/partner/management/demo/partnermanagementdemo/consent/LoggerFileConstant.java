29615db489ed63d62deaff340ed64b12193e5a0c
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2021-08-27 09:55:15 +0530

    Delete partner-management-demo directory

--- partner-management-demo/src/main/java/io/mosip/partner/management/demo/partnermanagementdemo/consent/LoggerFileConstant.java
+++
@@ -1,7 +0,0 @@
-package io.mosip.partner.management.demo.partnermanagementdemo.consent;
-
-
-public enum LoggerFileConstant {
-
-	SUCCESS
-}
--- partner-management-demo/src/main/java/io/mosip/partner/management/demo/partnermanagementdemo/service/PartnerCreationService.java
+++
@@ -1,8 +0,0 @@
-package io.mosip.partner.management.demo.partnermanagementdemo.service;
-
-import io.mosip.partner.management.demo.partnermanagementdemo.dto.PartnerDetailModel;
-import io.mosip.partner.management.demo.partnermanagementdemo.dto.ResponseModel;
-
-public interface PartnerCreationService {
-    public ResponseModel createPartner(PartnerDetailModel partnerModel);
-}
